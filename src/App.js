fc17ea896f2f2868e7594139b33a9e4f7cec297d
harryd57 <harryd57@yahoo.com>
2024-09-17 10:29:29 +0100

    App initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="h-[400px] bg-indigo-500 text-white">
+        Hello World
+      </div>
+    </React.Fragment>
   );
 }
 
