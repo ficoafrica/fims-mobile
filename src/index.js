628da582d8391993b12a9e037514134080302271
harryd57 <harryd57@yahoo.com>
2024-11-12 02:59:51 +0100

    Added favicons

--- src/index.js
+++ src/index.js
@@ -7,16 +7,15 @@ import { Provider } from 'react-redux'
 
 const redirectBasedOnScreenSize = () => {
   if (!window.matchMedia("(max-width: 768px)").matches) {
-    // Redirect for small screens (like mobile)
     window.location.href = "https://fims-web.vercel.app";
-    return false;
-  } else {
     return true;
+  } else {
+    return false;
   }
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-if (redirectBasedOnScreenSize()){
+if (!redirectBasedOnScreenSize()){
   root.render(
     <Provider store={store}>
       <App />
